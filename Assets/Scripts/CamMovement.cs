8450d3a2e7c67d287f05d32aa97e0c5d9e697ecd
Michael Gorkiewicz <michael.g@northernanvil.com>
2021-10-08 09:04:28 +0200

    Added offSet to the camera

--- Assets/Scripts/CamMovement.cs
+++ Assets/Scripts/CamMovement.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class CamMovement : MonoBehaviour
 {
     public Transform targetPlayer;
-    public float speedAcc = 1f;
-    public float speedAccInc = 0.01f;
-    public float smoothSpeeed = 10f;
-    public float camOffset = 0f;
+    [SerializeField]
+    private float speedAcc = 1f;
+    [SerializeField]
+    private float speedAccInc = 0.02f;
+    [SerializeField]
+    private float smoothSpeeed = 0.6f;
+    [SerializeField]
+    private float camOffset = 10f;
 
     private void FixedUpdate()
 
@@ -28,7 +32,7 @@ public class CamMovement : MonoBehaviour
 
     void followPlayer()
     {
-        Vector3 newPosition = new Vector3(targetPlayer.position.x, transform.position.y, transform.position.z);
+        Vector3 newPosition = new Vector3(targetPlayer.position.x + camOffset, transform.position.y, transform.position.z);
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, newPosition, smoothSpeeed * Time.deltaTime);
         transform.position = smoothedPosition;
         // transform.position = newPosition;
