1fc84dc9365768acd22fa321abfec92892993743
Johanna Johansson <johannajohansson642@gmail.com>
2021-10-10 20:54:29 +0200

    fixed "new player"

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,8 @@ public class Player : MonoBehaviour
   [SerializeField] private float checkRadius;
   [SerializeField] private LayerMask groundObject;
   [SerializeField] private Transform groundCheck;
-  
+
+  private Animator Ani;
   //adding reference to UI Manager
   private UIManager uiManager;
   
@@ -28,13 +29,17 @@ public class Player : MonoBehaviour
   private void Start()
   {
     uiManager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+    Ani = transform.GetChild(0).GetComponent<Animator>();
   }
 
   private void Update()
   {
     horizontalMovment = Input.GetAxis("Horizontal");
+    //Ani.SetBool("is");
+    
     if (Input.GetButtonDown("Jump") && isGrounded)
     {
+      Ani.SetBool("isJumping", true);
       isJumping = true;
       Debug.Log(isJumping);
       //FindObjectOfType<AudioManager>().Play("Jump");
@@ -76,7 +81,7 @@ public class Player : MonoBehaviour
 
   private void OnBecameInvisible()
   {
-    uiManager.GameOver();
+    //uiManager.GameOver();
   }
 
   #endregion
