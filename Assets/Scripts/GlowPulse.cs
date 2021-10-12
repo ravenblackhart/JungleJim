2a6102ec7a45f0da8657cd071423760727fbc574
Raven Blackhart <raven@cafiend.com>
2021-10-12 20:03:30 +0200

    Sprites Update & GlowPulse

---
+++ Assets/Scripts/GlowPulse.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GlowPulse : MonoBehaviour
+{
+    [SerializeField] private float GlowModerator = 1f;
+    [SerializeField] private float PulseSpeed = 2f;
+    private float glowSize; 
+    
+    void Update()
+    {
+        glowSize = 1 + (Mathf.Sin(Time.time / PulseSpeed) * GlowModerator);
+
+        transform.localScale = new Vector2(glowSize, glowSize);
+    }
+}
