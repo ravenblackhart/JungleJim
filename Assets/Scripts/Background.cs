5f4e448d90992dfc82628c0a60073a678c4f1d61
Johanna Johansson <johannajohansson642@gmail.com>
2021-10-12 19:54:18 +0200

    Added background working but we might want to change.

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private float lenght, startpos;
+
+    public GameObject cam;
+
+    public float parallaxEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x; // lenghts of the sprites
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float distance = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
+        if (temp > startpos +lenght)
+        {
+            startpos += lenght;
+        }
+        else if (temp < startpos -lenght)
+        {
+            startpos -= lenght;
+        }
+    }
+}
